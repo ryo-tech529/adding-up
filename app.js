d35da1391447b3128559fcb633921374df913919
ryo-tech529 <seminonakigoe@gmail.com>
2019-03-13 01:27:55 +0000

    ランキングの実装完了

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ rl.on('close',() => {
     const rankingArray = Array.from(prefectureDataMap).sort((pair1,pair2)=>{
         //console.log(pair1[0]);
         //console.log(pair2[0]);
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key,value])=>{
-        return key + ':' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map(([key,value],i)=>{
+        return (i+1) +'位: '+ key + ':' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
     });
 
     console.log(rankingStrings);
